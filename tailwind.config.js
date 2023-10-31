191292430b77a1213b243266c0c136c5761c2089
Mohammad Ravoof <140537466+mohammadravoof@users.noreply.github.com>
2023-10-31 23:29:45 +0530

    created the login screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+
+      },
+      colors:{
+        "neutral-1000":"#000000",
+        "twitter-blue-default":"#1D9BF0",
+        "stroke":"rgba(29, 155, 240, 0.24)",
+        "secondary":"rgba(255, 255, 255, 0.60)",
+      },
+    },
   },
   plugins: [],
 };
